d39965acaee8eb590435ad8f470a8d5bc7245a0f
amandev-art <amboo5112@gmail.com>
2024-11-30 13:54:07 +0530

    changes

---
+++ prj.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    # Base case: factorial of 0 or 1 is 1
+    if n == 0 or n == 1:
+        return 1
+    else:
+        # Recursive case: n * factorial of (n-1)
+        return n * factorial(n - 1)
+
+# Test the function
+number = 5
+print(f"Factorial of {number} is {factorial(number)}")
---
+++ togithub.py
@@ -0,0 +1,29 @@
+print("WELCOME TO FREE FIRE CONDITION CODE ")
+guns = input("enter the gun name:")
+print(guns)
+if guns == "mp40":
+ print("rush on enemies")
+
+if guns == "groza":
+ print("attack from distance")
+
+if guns == "thomson":
+ print("support teammates")
+
+if guns == "famas":
+ print("attack fast on distance")
+
+health = int(input("enter your health level"))
+print(health)
+
+if health == 100:
+ print("rush rush rush")
+
+if health == 80<=health:
+ print("heal with single medkit")
+
+if health == 60>=health:
+ print("dont rush heals first")
+
+
+ 
